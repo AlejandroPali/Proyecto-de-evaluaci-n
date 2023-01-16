16f622b66f83ac25f855fa05f2a35c06677544c4
ALEJANDRO PALIZADA SANCHEZ <1031584@horusur.corp>
2023-01-15 20:17:57 -0600

    agregando la funcion de raiz

--- Operaciones.py
+++ Operaciones.py
@@ -1,9 +1,12 @@
+import math
+
 def menu():
     print('**********CALCULADORA*********')
     print('Suma')
     print('resta')
     print('multiplicación')
     print('División')
+    print('raiz')
     print('Apagar')
 
 def suma(val1,val2):
@@ -26,14 +29,25 @@ def division(val1,val2):
     print('Resultado: '+ str(division))
     return
 
+def raiz(val1):
+    raiz = math.sqrt(int(val1))
+    print('Resultado: '+ str(raiz))
+    return 
+
 def datos (operacion):
-    val1 = input('Ingresa el primer Valor: ')
-    val2 = input('Ingresa el segundo Valor: ')
+    if operacion != 5:
+        val1 = input('Ingresa el primer Valor: ')
+        val2 = input('Ingresa el segundo Valor: ')
+    else :
+        val1 = input('Ingresa el primer Valor: ')
+
     if operacion == 1:
         suma(val1,val2)
     elif operacion == 2:
         resta(val1,val2)
     elif operacion == 3:
         multiplicacion(val1,val2)
-    else:
+    elif operacion == 4:
         division(val1,val2)
+    elif operacion == 5:
+        raiz(val1)
---
+++ examen.py
@@ -0,0 +1,22 @@
+#calculadora
+
+import Operaciones
+encendido=True
+accion=''
+
+while encendido:
+    Operaciones.menu()
+    accion = input('¿Qué desea realizar? ')
+
+    if accion.lower().lower() == 'suma':
+        Operaciones.datos(1)
+    elif accion.lower() == 'resta':
+        Operaciones.datos(2)
+    elif accion.lower() == 'multiplicacion':
+        Operaciones.datos(3)
+    elif accion.lower() == 'division':
+        Operaciones.datos(4)
+    elif accion.lower() == 'apagar':
+        encendido = False
+    else:
+        print('Ingresa una accion correcta (Sin acentos, ni numeros)')
